c0d5e02607a06742b2f6efb8f250fd4f6fc33f67
hindm <hindm@HINDABD>
2026-05-02 22:17:41 -0500

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using StudyBuddyFinal.Models;
+
+namespace StudyBuddyFinal1.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+    {
+        public DbSet<StudyBuddyFinal.Models.Course> Course { get; set; } = default!;
+    }
+}
---
+++ Models/Course.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudyBuddyFinal.Models
+{
+    public class Course
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string CourseName { get; set; }
+
+        public string Instructor { get; set; }
+    }
+}
